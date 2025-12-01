871aedfb8d714d19ffdb90f11ec47eaac2a7717b
rienkvdw <rienkvdwijk@gmail.com>
2025-12-01 15:27:46 +0100

    Probeersel voor deel 2 dat niet werkt (maar misschien ga ik een keer debuggen)

--- ass01/code.py
+++ ass01/code.py
@@ -11,8 +11,6 @@ with open('ass01/input.txt') as inputfile:    # input lezen en splitten in lines
     inputstring = inputfile.read()
     input = inputstring.splitlines()
 
-
-
 dirs = []
 amounts = []
 for lines in input:                     # splitten op de gehele set waar ik voor wil checken
@@ -24,16 +22,35 @@ ts = time.time()
 # part 1
 positions = []
 positions.append(50)
-for id,dir in enumerate(dirs):
-    if dir == "L":
-        positions.append(positions[-1] - amounts[id])
-    else:
-        positions.append(positions[-1] + amounts[id])
-
 zeroes = 0
-for position in positions:
-    if position%100 == 0:
+for idx,dir in enumerate(dirs):
+    
+    if dir == "L":
+        positions.append(positions[-1] - amounts[idx])
+    else:
+        positions.append(positions[-1] + amounts[idx])
+    if positions[-1]%100 == 0:
         zeroes += 1
-print("You hit the number zero " + str(zeroes) + " times")
+print("Part 1 cost " + str(time.time()-ts) + " seconds")
 
-print("Deel 1 kostte " + str(time.time()-ts) + " seconden")
+passes = 0
+positions_div = [position/100 for position in positions]
+positions_round = []
+for position in positions_div:
+    if position > 0:
+        positions_round.append(math.ceil(position))
+    else:
+        positions_round.append(math.floor(position))
+
+for idx in range(1,len(positions_round)):
+    if positions_round[idx] != positions_round[idx-1]:
+        if positions_round[idx] == 0 or positions_round[idx-1] == 0 or positions_round[idx] ^ positions_round[idx-1] < 0:
+            passes += abs(positions_round[idx-1] - positions_round[idx])-1
+        else:
+            passes += abs(positions_round[idx-1] - positions_round[idx])
+
+print("You hit the number zero " + str(zeroes) + " times")
+print("You passed the number zero " + str(passes) + " times")
+print("The total is " + str(zeroes + passes))
+
+print("Part 1 & 2 cost " + str(time.time()-ts) + " seconds")
