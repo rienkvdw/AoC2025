9ed97aa753b7ae0e863f35114321d3829cd30aef
rienkvdw <rienkvdwijk@gmail.com>
2025-12-09 17:12:18 +0100

    iets sneller (ongeveer 3 seconden), okay voor mij

--- ass09/code.py
+++ ass09/code.py
@@ -15,7 +15,6 @@ class Tile:
     def __init__(self, coords, id):
         self.pos = Coordinate(coords)
         self.id = id
-        self.posmap = Coordinate(coords)
     def __str__(self):
         return str((self.pos,self.id))
     def __repr__(self):
@@ -36,77 +35,45 @@ tiles = []
 for i in range(len(lines)):
     tiles.append(Tile([int(x) for x in lines[i].split(',')], i))
 
-rectangles = []
-for i in range(len(tiles)):
-    rectangle = []
-    for j in range(i):
-        rectangle.append(rectangles[j][i])
-    rectangle.append(1)
-    for j in range(i+1,len(tiles)):
-        rectangle.append(rectangleArea(tiles[i].pos,tiles[j].pos))
-    rectangles.append(rectangle)
-
-largest_rectangle1 = max([max(line) for line in rectangles])
-print("The value for part 1 is " + str(largest_rectangle1) + " which is " + str(largest_rectangle1 == 4750297200)) # correcte waarde is 4750297200
-t1 = time.time()
-
-# part 2
-# we gaan wat proberen. De getalletjes zijn te groot, dus we mappen ze naar kleinere getallen
-xs = set()
-ys = set()
-# eerst alle x en y coordinaten vinden
-for i in range(len(lines)):
-    coord = [int(x) for x in lines[i].split(',')]
-    xs.add(coord[0])
-    ys.add(coord[1])
-# dan die allemaal in een map gooien (met dict)
-xmap = {xcoord: idmap for idmap, xcoord in enumerate(sorted(xs))}
-ymap = {ycoord: idmap for idmap, ycoord in enumerate(sorted(ys))}
-# dan die waardes toevoegen aan posities van dingen
-for tile in tiles:
-    tile.posmap.x = xmap[tile.pos.x]
-    tile.posmap.y = ymap[tile.pos.y]
-
-# mapped_grid = [[0 for _ in range(len(xmap))] for _ in range(len(ymap))]
-# for i in range(len(tiles)):
-#     id1 = (i+1)%len(tiles)
-#     if tiles[id1].posmap.x > tiles[i].posmap.x:
-#         for j in range(tiles[i].posmap.x, tiles[id1].posmap.x):
-#             mapped_grid[tiles[i].posmap.y][j] = 1
-#     elif tiles[id1].posmap.x < tiles[i].posmap.x:
-#         for j in range(tiles[i].posmap.x, tiles[id1].posmap.x, -1):
-#             mapped_grid[tiles[i].posmap.y][j] = 1
-#     elif tiles[id1].posmap.y > tiles[i].posmap.y:
-#         for j in range(tiles[i].posmap.y, tiles[id1].posmap.y):
-#             mapped_grid[j][tiles[i].posmap.x] = 1
-#     else:
-#         for j in range(tiles[i].posmap.y, tiles[id1].posmap.y, -1):
-#             mapped_grid[j][tiles[i].posmap.x] = 1
-
+# dit gebruik ik bij deel 2, dus heb hier naartoe verplaatst zodat ik die andere niet meer nodig heb
 areas = []
 for i in range(len(tiles)-1):
     for j in range(i+1,len(tiles)):
         areas.append((rectangleArea(tiles[i].pos,tiles[j].pos),i,j))
-areas_sorted = sorted(areas, key = lambda x : x[0])
-for a in range(len(areas_sorted)-1, -1, -1):
-    xmin = min([tiles[areas_sorted[a][1]].pos.x,tiles[areas_sorted[a][2]].pos.x])
-    xmax = max([tiles[areas_sorted[a][1]].pos.x,tiles[areas_sorted[a][2]].pos.x])
-    ymin = min([tiles[areas_sorted[a][1]].pos.y,tiles[areas_sorted[a][2]].pos.y])
-    ymax = max([tiles[areas_sorted[a][1]].pos.y,tiles[areas_sorted[a][2]].pos.y])
+areas_sorted = sorted(areas, key = lambda x : x[0], reverse=True)
+
+largest_rectangle1 = areas_sorted[0][0]
+print("The value for part 1 is " + str(largest_rectangle1) + " which is " + str(largest_rectangle1 == 4750297200)) # correcte waarde is 4750297200
+t1 = time.time()
+
+# part 2
+# de slimme dingen die ik had bedacht waren niet nuttig :(
+
+# we gaan door alle areas heen
+for area in areas_sorted:
+    corner0 = tiles[area[1]]
+    corner1 = tiles[area[2]]
+    xmin, xmax = min([corner0.pos.x,corner1.pos.x]), max([corner0.pos.x,corner1.pos.x])
+    ymin, ymax = min([corner0.pos.y,corner1.pos.y]), max([corner0.pos.y,corner1.pos.y])
 
     cross = False
-    for i in range(len(tiles)):
+    # we beginnen bij de id van corner0, want grote kans dat er in de buurt van een corner wat dingen gebeuren die het verneuken voor die corner
+    for i in range(corner0.id, len(tiles) + corner0.id):
+        id0 = i%len(tiles)
         id1 = (i+1)%len(tiles)
-        if (tiles[i].pos.x == tiles[id1].pos.x) and not (tiles[i].pos.x <= xmin or tiles[i].pos.x >= xmax):
-            if not ((tiles[i].pos.y <= ymin and tiles[id1].pos.y <= ymin) or (tiles[i].pos.y >= ymax and tiles[id1].pos.y >= ymax)):
+        tile0_pos = tiles[id0].pos
+        tile1_pos = tiles[id1].pos
+        # hier is heel wat geoptimaliseerd qua checks, dus leesbaar is het niet meer
+        if (tile0_pos.x == tile1_pos.x):
+            if (xmin < tile0_pos.x < xmax) and (tile0_pos.y > ymin or tile1_pos.y > ymin) and (tile0_pos.y < ymax or tile1_pos.y < ymax):
                 cross = True
                 break
-        elif (tiles[i].pos.y == tiles[id1].pos.y) and not (tiles[i].pos.y <= ymin or tiles[i].pos.y >= ymax):
-            if not ((tiles[i].pos.x <= xmin and tiles[id1].pos.x <= xmin) or (tiles[i].pos.x >= xmax and tiles[id1].pos.x >= xmax)):
+        elif (tile0_pos.y == tile1_pos.y):
+            if (ymin < tile0_pos.y < ymax) and (tile0_pos.x > xmin or tile1_pos.x > xmin) and (tile0_pos.x < xmax or tile1_pos.x < xmax):
                 cross = True
                 break
     if not cross:
-        largest_rectangle2 = areas_sorted[a][0]
+        largest_rectangle2 = area[0]
         break
 
 print("The value for part 2 is " + str(largest_rectangle2) + " which is " + str(largest_rectangle2 == 1578115935)) # correcte waarde is 1578115935
@@ -117,5 +84,5 @@ print("Timing: Inputs = " + str(int((tr-ts)*10**6//1)) + " us;" +
       " Part 2 = " + str(int((t2-t1)*10**3//1)) + " ms;" +
       " Total time = " + str(int((t2-ts)*10**3//1)) + " ms")
 
-# average van  run is ongeveer
-# kost  us,  us,  us, us
+# average van 1 run is ongeveer
+# kost 100 us, 120 ms, 2900 ms, 3100 ms
