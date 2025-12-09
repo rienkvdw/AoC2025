0ef21b1cfd501aef5e401cc3937bde8a2c938f6d
rienkvdw <rienkvdwijk@gmail.com>
2025-12-09 13:12:55 +0100

    begonnen met optimalisatie voor deel 2, weet nog niet hoe ik ga checken

--- ass09/code.py
+++ ass09/code.py
@@ -15,6 +15,7 @@ class Tile:
     def __init__(self, coords, id):
         self.pos = Coordinate(coords)
         self.id = id
+        self.posmap = Coordinate(coords)
     def __str__(self):
         return str((self.pos,self.id))
     def __repr__(self):
@@ -46,11 +47,25 @@ for i in range(len(tiles)):
     rectangles.append(rectangle)
 
 largestRectangle = max([max(line) for line in rectangles])
-print("The value for part 1 is " + str(largestRectangle) + " which is " + str(largestRectangle == 4750297200)) # correcte waarde is 
+print("The value for part 1 is " + str(largestRectangle) + " which is " + str(largestRectangle == 4750297200)) # correcte waarde is 4750297200
 t1 = time.time()
 
 # part 2
-
+# we gaan wat proberen. De getalletjes zijn te groot, dus we mappen ze naar kleinere getallen
+xs = set()
+ys = set()
+# eerst alle x en y coordinaten vinden
+for i in range(len(lines)):
+    coord = [int(x) for x in lines[i].split(',')]
+    xs.add(coord[0])
+    ys.add(coord[1])
+# dan die allemaal in een map gooien (met dict)
+xmap = {xcoord: idmap for idmap, xcoord in enumerate(sorted(xs))}
+ymap = {ycoord: idmap for idmap, ycoord in enumerate(sorted(ys))}
+# dan die waardes toevoegen aan posities van dingen
+for tile in tiles:
+    tile.posmap.x = xmap[tile.pos.x]
+    tile.posmap.y = ymap[tile.pos.y]
 
 
 print("The total for part 2 is " + str("empty")) # correcte waarde is 
@@ -58,7 +73,7 @@ t2 = time.time()
 
 print("Timing: Inputs = " + str(int((tr-ts)*10**6//1)) + " us;" +
       " Part 1 = " + str(int((t1-tr)*10**3//1)) + " ms;" +
-      " Part 2 = " + str(int((t2-t1)*10**6//1)) + " us;" +
+      " Part 2 = " + str(int((t2-t1)*10**3//1)) + " ms;" +
       " Total time = " + str(int((t2-ts)*10**3//1)) + " ms")
 
 # average van  run is ongeveer
