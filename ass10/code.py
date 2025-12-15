336802896fe0696c0e4133201265f71c01f31c9f
rienkvdw <rienkvdwijk@gmail.com>
2025-12-15 12:43:23 +0100

    basis bestanden dag 10, 11 en 12

---
+++ ass10/code.py
@@ -0,0 +1,29 @@
+# assignment 
+import time
+
+ts = time.time()
+
+
+# uitlezen text bestandje
+with open('ass00/input.txt') as inputfile:    # input lezen en splitten in lines
+    inputstring = inputfile.read()
+
+tr = time.time()
+# part 1
+
+print("The total for part 1 is " + str("empty")) # correcte waarde is 
+t1 = time.time()
+# part 2
+
+
+
+print("The total for part 2 is " + str("empty")) # correcte waarde is 
+t2 = time.time()
+
+print("Timing: Inputs = " + str(int((tr-ts)*10**6//1)) + " us;" +
+      " Part 1 = " + str(int((t1-tr)*10**6//1)) + " us;" +
+      " Part 2 = " + str(int((t2-t1)*10**6//1)) + " us;" +
+      " Total time = " + str(int((t2-ts)*10**6//1)) + " us")
+
+# average van  run is ongeveer
+# kost  us,  us,  us, us
